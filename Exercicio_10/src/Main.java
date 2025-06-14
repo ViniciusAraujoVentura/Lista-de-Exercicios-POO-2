7b97f0ffa3c43934adda3f6115fa86c0cdda814b
ViniciusAraujoVentura <vinicius2015aaraujoo@gmail.com>
2025-06-13 23:42:27 -0300

    Create Main.java

---
+++ Exercicio_10/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite dois numeros inteiros:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a % b == 0 || b % a == 0) {
+            System.out.println("Sao multiplos");
+        } else {
+            System.out.println("Nao sao multiplos");
+        }
+
+        sc.close();
+    }
+}
