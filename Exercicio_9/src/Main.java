054f4f27dd89bd14d3be1e58e911b34def655375
ViniciusAraujoVentura <vinicius2015aaraujoo@gmail.com>
2025-06-13 23:41:53 -0300

    Create Main.java

---
+++ Exercicio_9/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Codigo do produto comprado: ");
+        int codigo = sc.nextInt();
+
+        System.out.print("Quantidade comprada: ");
+        int quantidade = sc.nextInt();
+
+        double preco;
+
+        if (codigo == 1) {
+            preco = 5.00;
+        } else if (codigo == 2) {
+            preco = 3.50;
+        } else if (codigo == 3) {
+            preco = 4.80;
+        } else if (codigo == 4) {
+            preco = 8.90;
+        } else if (codigo == 5) {
+            preco = 7.32;
+        } else {
+            System.out.println("Código inválido.");
+            sc.close();
+            return;
+        }
+
+        double total = preco * quantidade;
+        System.out.printf("Valor a pagar: R$ %.2f%n", total);
+
+        sc.close();
+    }
+}
